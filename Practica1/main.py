7916543576242203f8e587d41b94f54c25b85903
gerard-medina <92846611+gerard-medina@users.noreply.github.com>
2023-11-09 17:22:55 +0100

    re lento che

--- Practica1/main.py
+++ Practica1/main.py
@@ -14,8 +14,8 @@ test_path = 'dat/fashion-mnist_test.csv'
 # Dades entrenament
 ds_train = pd.read_csv(train_path)
 
-x_train = ds_train.iloc[:, 1:]  # Píxeles imágenes
-y_train = ds_train.iloc[:, 0]  # Labels en la primera columna
+x_train = ds_train.iloc[:, 1:]  # Pixels imatges
+y_train = ds_train.iloc[:, 0]  # Labels primera columna
 
 scaler = StandardScaler()
 x_train_scaled = scaler.fit_transform(x_train)
@@ -23,26 +23,26 @@ x_train_scaled = scaler.fit_transform(x_train)
 # Dades test
 ds_test = pd.read_csv(train_path)
 
-x_test = ds_test.iloc[:, 1:]  # Píxeles imágenes
-y_test = ds_test.iloc[:, 0]  # Labels en la primera columna
+x_test = ds_test.iloc[:, 1:]  # Pixels imatges
+y_test = ds_test.iloc[:, 0]  # Labels primera columna
 
 x_test_scaled = scaler.fit_transform(x_test)
 
 param_grid = {
-    'C': [0.1, 1, 10],             # Regularization parameter
-    'kernel': ['linear', 'rbf'],    # Kernel type
-    'gamma': [0.001, 0.01, 0.1]    # Kernel coefficient
+    'C': [0.1, 1, 10],             # Parametre de regularitzacio
+    'kernel': ['linear', 'rbf'],    # falta un kernel
+    'gamma': [0.001, 0.01, 0.1]    # Coeficient de Kernel
 }
 
 svm = SVC()
 grid_search = GridSearchCV(svm, param_grid, cv=5, verbose=1, n_jobs=-1)
 grid_search.fit(x_train_scaled, y_train)
 
-# Todo el dataset con el mejor estimador
+# Dataset amb el millor estimador
 best_svm_model = grid_search.best_estimator_
 best_svm_model.fit(x_train_scaled, y_train)
 
-# Predicción
+# Prediccio
 y_pred = best_svm_model.predict(x_test)
 
 # Avaluacio
