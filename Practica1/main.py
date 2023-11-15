be2ce87829d8207309f5b0ab71ca31c8b8e60630
gerard-medina <gerardsonrosseo@hotmail.es>
2023-11-15 22:53:00 +0100

    paisatges

--- Practica1/main.py
+++ Practica1/main.py
@@ -1,52 +1,68 @@
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import SGDClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
+import os
+from sklearn.calibration import LabelEncoder
+from sklearn.discriminant_analysis import StandardScaler
 from sklearn.svm import SVC
-import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix
+from skimage import io
+from skimage.transform import resize
+from skimage.color import rgb2gray
+import numpy as np
 
-train_path = 'dat/fashion-mnist_train.csv'
-test_path = 'dat/fashion-mnist_test.csv'
+# Step 1: Load and Preprocess the Image Data
+def load_and_preprocess(folder_path, target_size=(64, 64)):
+    images = []
+    labels = []
+    
+    for label in os.listdir(folder_path):
+        label_path = os.path.join(folder_path, label)
+        
+        for image_name in os.listdir(label_path):
+            image_path = os.path.join(label_path, image_name)
+            image = io.imread(image_path)
+            
+            # Preprocess the image (resize, convert to grayscale, etc.)
+            image = resize(image, target_size)
+            # Check if the image is already grayscale
+            if image.ndim == 2:
+                # Grayscale image
+                images.append(image.flatten())
+            else:
+                # Convert color image to grayscale
+                gray_image = rgb2gray(image)
+                images.append(gray_image.flatten())
 
-# Dades entrenament
-ds_train = pd.read_csv(train_path)
+            labels.append(label)
+    
+    return np.array(images), np.array(labels)
 
-x_train = ds_train.iloc[:, 1:]  # Pixels imatges
-y_train = ds_train.iloc[:, 0]  # Labels primera columna
+# Step 2: Load and Label the Data
+train_path = 'dat/train/'
+test_path = 'dat/test/'
+X_train, y_train = load_and_preprocess(train_path)
+print("Imatges train processades")
+X_test, y_test = load_and_preprocess(test_path)
+print("Imatges test processades")
 
+# Step 3: Scale the Data
 scaler = StandardScaler()
-x_train_scaled = scaler.fit_transform(x_train)
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+label_encoder = LabelEncoder()
+y_train = label_encoder.fit_transform(y_train)
+y_test = label_encoder.fit_transform(y_test)
+print("Imatges escalades")
 
-# Dades test
-ds_test = pd.read_csv(train_path)
+# Step 4: Train the SVC Model
+kernels = ['linear', 'rbf', 'poly']
 
-x_test = ds_test.iloc[:, 1:]  # Pixels imatges
-y_test = ds_test.iloc[:, 0]  # Labels primera columna
+for kernel in kernels:
+    print(f"TEST KERNEL: {kernel}")
+    svm = SVC(kernel=kernel)
+    svm.fit(X_train_scaled, y_train)
+    y_pred = svm.predict(X_test_scaled)
 
-x_test_scaled = scaler.fit_transform(x_test)
-
-param_grid = {
-    'C': [0.1, 1, 10],             # Parametre de regularitzacio
-    'kernel': ['linear', 'rbf'],    # falta un kernel
-    'gamma': [0.001, 0.01, 0.1]    # Coeficient de Kernel
-}
-
-svm = SVC()
-grid_search = GridSearchCV(svm, param_grid, cv=5, verbose=1, n_jobs=-1)
-grid_search.fit(x_train_scaled, y_train)
-
-# Dataset amb el millor estimador
-best_svm_model = grid_search.best_estimator_
-best_svm_model.fit(x_train_scaled, y_train)
-
-# Prediccio
-y_pred = best_svm_model.predict(x_test)
-
-# Avaluacio
-cf_matrix = confusion_matrix(y_test, y_pred)
-print(cf_matrix)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy: ", accuracy)
+    # Step 5: Evaluate the Model
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    print("Confusion matrix:\n", cf_matrix)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy: ", accuracy)
